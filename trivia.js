1154207463f6e0dd1c028b6094d8d33449edc095
Ricardo Rodriguez <ricardorod1147@gmail.com>
2021-10-13 21:03:34 -0500

    Terminado

--- trivia.js
+++ trivia.js
@@ -4,6 +4,11 @@ const dificultad = document.getElementById("difficulty");
 const category = document.getElementById("category");
 const typeQuestions = document.getElementById("typeQuestions");
 const boton = document.getElementById("submitSearch");
+const formTrivia = document.getElementById("form-trivia");
+const container = document.getElementsByClassName("container");
+const containerPreguntas = document.getElementById("renderPreguntas");
+const holaa = document.getElementById("botoN");
+
 
 let arrNum = [];
 let arrdi = [];
@@ -23,13 +28,6 @@ runCategory(result);
 runTypeQuestions(result);
 }
 // EVENTOS DE LLAMADO A FUNCIONES
-
-// handleSearchQuestIONS();
-
-const handleSearchQuestIONS = () =>{
-    
-}
-
 // FUNCION NUMERO DE PREGUNTAS
 const runNumQuestions = difi =>{
     let i = 1;
@@ -40,8 +38,230 @@ const runNumQuestions = difi =>{
     arrNum.forEach(cont => {
         numPreguntas.innerHTML += `<option value="${cont}">${cont}</option>`;
     })
+    }   
+// handleSearchQuestIONS();
+const handleSearchQuestIONS = e =>{
+    e.preventDefault();
+    // console.log(`${numPreguntas.value} la segunda es  ${category.value}  
+    // la dificutad es    ${dificultad.value}   y el tipo de pregunta es  ${typeQuestions.value} `)
+    const Arrcategorias = [
+        'General Knowledge',
+        'Entertainment: Books',
+        'Entertainment: Film',
+        'Entertainment: Music',
+        'Entertainment: Musicals & Theatres',
+        'Entertainment: Television',
+        'Entertainment: Video Games',
+        'Entertainment: Board Games',
+        'Science & Nature',
+        'Science: Computers',
+        'Science: Mathematics',
+        'Mythology',
+        'Sports',
+        'Geography',
+        'History',
+        'Politics',
+        'Art',
+        'Celebrities',
+        'Animals',
+        'Vehicles',
+        'Entertainment: Comics',
+        'Science: Gadgets',
+        'Entertainment: Japanese Anime & Manga',
+        'Entertainment: Cartoon & Animations',
+        ];
+        let apiDinamic = ""; 
+    let cateAPI
+    const numeroCate = retorno(Arrcategorias);
+        if(numeroCate>0){cateAPI = "&category="+numeroCate;
+        }else{cateAPI = "";
+        }
+        const returDificultad = retornoDificultad();
+        const returType = returnTipo();
+        apiDinamic = `https://opentdb.com/api.php?amount=${numPreguntas.value}${cateAPI}${returDificultad}${returType}`;
+        renderQuestions(apiDinamic);
+    
+}
+//RENDERIZAR PREGUNTAS BAJO LA URL DE API DINAMICA
+const renderQuestions = async api =>{
+    const fetchDos = await fetch(api);
+    const resulDinamic = await fetchDos.json();
+    renderFormat(resulDinamic);
+}
+
+const renderFormat=(objetoRender)=>{
+    let count = 0;
+    let arrayQuestions = objetoRender.results;
+    let incorrectQuestion = arrayQuestions[count].incorrect_answers;
+    let puntajeUno = 0;
+    let arrRespuestas = [];
+    console.log(arrayQuestions);
+    
+    containerPreguntas.innerHTML = ``;
+
+    if(incorrectQuestion.length>=2){ 
+        let render = ()=> { 
+            let resCorrect = arrayQuestions[count].correct_answer;
+            arrRespuestas = [incorrectQuestion[2],incorrectQuestion[1],incorrectQuestion[0],arrayQuestions[count].correct_answer];
+            console.log(arrRespuestas);
+            arrRes = [...arrRespuestas];
+            arrRes = arrRes.sort(function() {return Math.random() -0.5});
+            console.log(arrRes);
+        containerPreguntas.innerHTML =`<div class="active">
+        <div class="zoneQuestion">
+        <h2>Pregunta  ${count+1}</h2>
+        <p>${arrayQuestions[count].question}</p>
+        </div>
+        <div class="zoneAnswer">
+        <input type="button" id="resUno" value="${arrRes[2]}" class="botonRespuesta">
+        <input type="button" id="resDos" value="${arrRes[1]}" class="botonRespuesta">
+        <input type="button" id="resTres" value="${arrRes[0]}" class="botonRespuesta">
+        <input type="button" id="resCuatro" value="${arrRes[3]}" class="botonRespuesta">
+        <input type="button" value="Siguiente" class="botonNext" id="holaaa">
+        </div>
+        </div>`;
+
+
+                const resUno = document.getElementById("resUno");
+                const resDos = document.getElementById("resDos");
+                const resTres = document.getElementById("resTres");
+                const resCuatro = document.getElementById("resCuatro");
+
+                console.log(resCorrect);
+
+                const handleUno = (valor) =>{
+                        if(resCorrect == valor){
+                            puntajeUno++;
+                            console.log(`El puntaje es ${puntajeUno} el valor es ${valor}`)
+                            saludo();
+                        }else{
+                            console.log("Incorrecta");
+                            saludo();
+                        }
+                }
+
+                resUno.onclick = () =>{
+                    handleUno(resUno.value);
+                }
+                resDos.onclick = () =>{
+                    handleUno(resDos.value);
+                }
+                resTres.onclick = () =>{
+                    handleUno(resTres.value);
+                }
+                resCuatro.onclick = () =>{
+                    handleUno(resCuatro.value);
+                }
+
+
+
+// FUNCION PARA DAR SIGUIENTE PREGUNTA
+        const btnext = document.getElementById("holaaa");
+        const saludo = () => {
+            
+        if(count<(arrayQuestions.length-1)){
+            count++;
+            render();
+        }else{
+            containerPreguntas.innerHTML =
+            `<div class="active">
+            <div class="zoneQuestion">
+            <h2>Su puntaje es  ${puntajeUno}</h2>
+            <p>Fin del juego</p>
+            </div>
+            <div class="zoneAnswer">
+            </div>`
+        }
+
+        }
+        btnext.onclick = saludo;
+// FIN FUNCION PARA DAR SIGUIENTE PREGUNTA
     }
+    render();
+              
+
+    }else{
+        let puntajeCorrect = 0;
+        
+// FUNCION PARA RENDERIZAR EN EL CASO QUE SEA DEL TIPO BOOLEAN
+        let renderDos = () => {
+            let respuestaUno = incorrectQuestion[0];
+            let respuestaDos = arrayQuestions[count].correct_answer;
+        containerPreguntas.innerHTML =`<div class="active">
+        <div class="zoneQuestion">
+        <p>Pregunta${count+1}</p>
+        <p>${arrayQuestions[count].question}</p>
+        </div>
+        <div class="zoneAnswer">
+        <input type="button" id="resUno" value="${respuestaUno}" class="botonRespuesta">
+        <input type="button"  id="resDos" value="${respuestaDos}" class="botonRespuesta">
+        </div>
+        </div> `;
+        console.log(`La mala es ${incorrectQuestion[0]} la buena es ${arrayQuestions[count].correct_answer}`);
+       
+// FIN FUNCION RENDERIZAR EN EL CASO QUE SEA DEL TIPO BOOLEAN
+
+// RESPUESTA CORRECTA
+        
+                                const ansOne = document.getElementById("resUno");
+                                const ansTwo = document.getElementById("resDos");
+
+                                const handlecorrect = (valor) =>{  
+                                  if(valor==respuestaDos){
+                                    puntajeCorrect++;
+                                    siguienteDos();
+                                  }else{
+                                    siguienteDos();
+                                  }
+                            }
+
+                                ansOne.onclick = () => { handlecorrect(ansOne.value);
+                                }
+                                ansTwo.onclick = () => { handlecorrect(ansTwo.value);
+                                }
+
+// FIN  RESPUESTA CORRECTA
+
+                                   
+
+// FIN RESPUESTA INCORRECTA
+
+
+
+// RESPUESTA INCORRECTA
+
+
+        const siguienteDos = () => {
+            
+        if(count<(arrayQuestions.length-1)){
+            count++;
+            renderDos();
+        }else{
+            containerPreguntas.innerHTML =
+            `<div class="active">
+            <div class="zoneQuestion">
+            <h2>Su puntaje es  ${puntajeCorrect}</h2>
+            <p>Fin del juego</p>
+            </div>
+            <div class="zoneAnswer">
+            </div>`
+        }
+
+        
+        }
+
+    }
+    renderDos();
+}
+}
+
+
+
 // FUNCION DIFICULTAD
+const retornoDificultad = () => {
+    if(dificultad.value!="AnyDifficulty"){return "&difficulty="+dificultad.value;
+    }else{return "";
+    }}
 const runDificult = difi =>{
 difi.results.forEach(difArr => {
     if(!(arrdi.includes(difArr.difficulty))){
@@ -51,26 +271,35 @@ difi.results.forEach(difArr => {
 dificultad.innerHTML = `<option value="AnyDifficulty">Any Difficulty</option>`;
 arrdi.forEach(cont => {
 dificultad.innerHTML += `<option value="${cont}">${cont}</option>`;
-})
-}
+})}
 // FUNCION CATEGORIA
+const retorno = (categorias) => {
+    if(categorias.includes(category.value)){let cateNumero = (categorias.indexOf(category.value))+9;
+        return cateNumero;
+       }else{cateNumero = "";
+           return cateNumero;
+       }}
 const runCategory = difi =>{
     difi.results.forEach(difArr => {
         if(!(arrCat.includes(difArr.category))){
             arrCat.push(difArr.category); 
-        }
-    });
+        }});
     category.innerHTML = `<option value="AnyCategory">AnyCategory</option>`;
     arrCat.forEach(cont => {
         category.innerHTML += `<option value="${cont}">${cont}</option>`;
+        // console.log(cont);
     })
-    console.log(arrCat);
+    // console.log(arrCat);
     }
 // FUNCION TIPO DE PREGUNTA
+
+const returnTipo = () => { 
+    if(typeQuestions.value!="AnyType"){return "&type="+typeQuestions.value;
+    }else{return "";
+    }}
 const runTypeQuestions = difi =>{
     difi.results.forEach(difArr => {
-        if(!(arrype.includes(difArr.type))){
-            arrype.push(difArr.type); 
+        if(!(arrype.includes(difArr.type))){arrype.push(difArr.type); 
         }
     });
     typeQuestions.innerHTML = `<option value="AnyType">Any Type</option>`;
@@ -79,4 +308,7 @@ const runTypeQuestions = difi =>{
     })
     }
 document.addEventListener("DOMContentLoaded", fetchAPI);
+formTrivia.onsubmit = handleSearchQuestIONS;
+
+
 
